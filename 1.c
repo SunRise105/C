f9560957c827bab8c9f124a16a6f99f782343387
SunRise105 <a.makedonia@yandex.ru>
2024-02-16 20:16:22 +0300

    pered moduls

--- 1.c
+++ 1.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main (void)
-
+int hello(char *nick_name)
 {
-    int goblin_gold = 15;
-    int player_gold = 0;
-    char action;
-    int hp_goblin;
-    char nick_name[9]; 
-
     printf("What is your name?\n");
     scanf("%8[^\n]",nick_name);
     printf("Hi, %s\n", nick_name);
     while(getchar() != '\n');
+    return 0;
+}
+int battle(int player_gold)
+{
+    int goblin_gold = 15;
+for (int hp_goblin = 3; hp_goblin >= 0;)
+ {
+     hp_goblin = hp_goblin - 1;
+    if (hp_goblin > 0)
+        printf("You punch Sanya. Sanya HP: %d\n", hp_goblin);
+    if (hp_goblin == 0)
+        {
+        printf("You killed Sanya.\n");
+        player_gold = player_gold + goblin_gold;
+        printf("You found %d gold. You have total hold %d\n", goblin_gold, player_gold);
+        break;
+        }
+        
+ }
+ return player_gold;
+}
+int main (void)
 
-    printf("You attacked by Sanya(3HP). 'A'ttack or 'R'un\n", hp_goblin);
+{
+    int player_gold = 0;
+    char action;
+    char nick_name[9]; 
+
+hello(nick_name);
+    printf("You attacked by Sanya(3HP). 'A'ttack or 'R'un\n");
     action = getchar();
 
     while (action != 'r')
     {
-            if (action == 'a' || action == 'A')
+         if (action == 'a' || action == 'A')
         {
-            for (hp_goblin = 3; hp_goblin >= 0;)
-            {
-                hp_goblin = hp_goblin - 1;
-                if (hp_goblin > 0)
-                    printf("You punch Sanya. Sanya HP: %d\n", hp_goblin);
-                if (hp_goblin == 0)
-                {
-                    printf("You killed Sanya.\n");
-                    player_gold = player_gold + goblin_gold;
-                    printf("You found %d gold. You have total hold %d\n", goblin_gold, player_gold);
-                    break;
-                }
-                
-            }
+           player_gold = battle(player_gold);
         }
 
   
@@ -42,6 +51,7 @@ int main (void)
     getchar();
     action = getchar();
         }
-    if (action == 'r') printf("You've decided to run away!\n");
+    if (action == 'r') printf("You earned %d gold! Good job!\n", player_gold);
+    _getch();
     return 0;
 }
